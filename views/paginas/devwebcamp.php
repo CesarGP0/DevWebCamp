cb2ca9fc1a8c55233215e5e5959deaa14d79e01c
unknown <cesar_050500@hotmail.com>
2024-06-17 15:53:38 -0600

    Deployment

---
+++ views/paginas/devwebcamp.php
@@ -0,0 +1,21 @@
+<main class="devwebcamp">
+    <h2 class="devwebcamp__heading"><?php echo $titulo; ?></h2>
+
+    <p class="devwebcamp__descripcion">Conoce la conferencia mas importante de Latinoamérica</p>
+
+    <div class="devwebcamp__grid">
+        <div class="devwebcamp__iamgen">
+            <picture>
+                <source srcset="build/img/sobre_devwebcamp.avif" type="image/avif">
+                <source srcset="build/img/sobre_devwebcamp.webp" type="image/webp">
+                <img loading="lazy" width="200" height="300" src="build/img/sobre_devwebcamp.jpg" alt="Imagen DevWebcamp">
+            </picture>
+        </div>
+
+        <div class="devwebcamp__contenido">
+            <p class="devwebcamp__texto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad aperiam, hic rem nostrum impedit perspiciatis sit voluptas veniam cumque! In velit officiis, est provident asperiores molestiae praesentium fuga cumque alias.</p>
+            <p class="devwebcamp__texto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad aperiam, hic rem nostrum impedit perspiciatis sit voluptas veniam cumque! In velit officiis, est provident asperiores molestiae praesentium fuga cumque alias.</p>
+        </div>
+
+    </div>
+</main>
